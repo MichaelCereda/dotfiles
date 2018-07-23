e1ade437265aa1b72071fef2cdbf0912b9c00319
Michael Cereda <michael.cereda@gmail.com>
2018-07-23 16:23:29 -0400

    Cleanup scripts/packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
-sudo pacman -S $(cat packages)
-sudo yaourt -S $(cat packages-aur)
+#!/bin/bash 
+
+#sudo pacman -S $(cat packages)
+#sudo yaourt -S $(cat packages-aur)
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
